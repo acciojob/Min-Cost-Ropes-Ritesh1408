0510762f83b045c686530c06e300fd3c2d2b82ef
RITESH ROUSHAN KUMAR <78962046+Ritesh1408@users.noreply.github.com>
2025-02-15 18:32:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-	
-  
+const MinPriorityQueue = require('@datastructures-js/priority-queue').MinPriorityQueue;
+
+function mincost(arr) {
+    if (arr.length === 0) return 0;
+
+    let pq = new MinPriorityQueue(); 
+    let totalCost = 0;
+
+    // Insert all elements into the min-heap
+    arr.forEach(num => pq.enqueue(num));
+
+    while (pq.size() > 1) {
+        // Extract two smallest ropes
+        let first = pq.dequeue().element;
+        let second = pq.dequeue().element;
+        
+        let cost = first + second;
+        totalCost += cost;
+
+        pq.enqueue(cost);
+    }
+
+    return totalCost;
 }
 
-module.exports=mincost;
+module.exports = mincost;
